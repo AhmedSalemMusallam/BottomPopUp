243ad2204d9cfe1d671baac95b526ca12e1752ac
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-02-11 01:06:42 +0200

    Bottom Custom Popup Completed - Commit

---
+++ BottomPopUp/BottomPopUp/Controllers/CustomPopUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CustomPopUpViewController.swift
+//  BottomPopUp
+//
+//  Created by Ahmed Salem on 11/02/2023.
+//
+
+import UIKit
+
+class CustomPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+    
+
+    
+
+}
--- BottomPopUp/BottomPopUp/Controllers/ViewController.swift
+++ BottomPopUp/BottomPopUp/Controllers/ViewController.swift
@@ -11,9 +11,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+        
+        
     }
 
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        fireBottomCustomViews()
+    }
+    
 
+    func fireBottomCustomViews()
+    {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondVC = storyboard.instantiateViewController(identifier: "CustomPopUpViewController")
+
+        if let sheet = secondVC.sheetPresentationController{
+
+            sheet.preferredCornerRadius = 10
+
+            sheet.detents = [.custom(resolver: {
+                context in
+                0.1 * context.maximumDetentValue
+            }), .large()]
+
+        }
+        
+        present(secondVC, animated: true)
+    }
 }
 
